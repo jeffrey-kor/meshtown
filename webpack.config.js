65e2599c510d3be27344e103b8d74f2b062ffa69
jeff.cofos <wjdrlrkdl3@gmail.com>
2021-11-17 19:07:09 +0900

    [feature] Typescript config - modified .babelrc - modified .webpack.config.js - viewd hello world on web page

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require("path");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const port = Number(process.env.PORT) || 4000;
 require("dotenv").config();
@@ -6,24 +9,36 @@ require("dotenv").config();
 module.exports = {
 
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     filename: "bundle.[hash].js"
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|mjs|ts|tsx)$/,
         exclude: /node_modules/,
-        use: {
+        use: [{
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              ["@babel/preset-env", { modules: "commonjs" }],
+              "@babel/preset-typescript",
+              "@babel/preset-react",
+            ],
             cacheDirectory: true,
-            plugins: ["react-hot-loader/babel"]
+            plugins: [
+              "react-hot-loader/babel",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-syntax-dynamic-import",
+              "@babel/plugin-transform-runtime",
+              "@babel/plugin-transform-typescript"
+            ]
           }
-        },
+        }],
       },
+
       {
         test: /\.html$/,
         use: {
@@ -33,17 +48,28 @@ module.exports = {
           }
         }
       },
+
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
     ],
   },
+
+  resolve: {
+    modules: [path.join(__dirname), "src", "node_modules"],
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
-    })
+      filename: "index.html"
+    }),
+    new ForkTsCheckerWebpackPlugin(),
   ],
+
+  devtool: "source-map",
   devServer: {
     host: "localhost",
     port: port,
